caf2d0a455068f5d637a1b138d5e1b09813b25ee
ElysBactista <elyscamilabatista@gmail.com>
2025-12-11 19:02:10 -0400

    feat: Estructura inicial de clase AnalistaReportes
    Co-authored-by: <ElysBatista>

---
+++ src/ProyectoParaleloFinal/AnalistaReportes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProyectoFinalModular
+{
+    public class AnalistaReportes
+    {
+        public static void MostrarInformeEjecutivo(ResultadoPrediccion[] resultados)
+        {
+        }
+    }
+}
