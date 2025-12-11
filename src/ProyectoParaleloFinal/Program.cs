2f93541017128a2d0436c4ff346df5101040e665
wilmar sanchez <wilmarsanchez347@gmail.com>
2025-12-11 17:04:56 -0400

    feat: implementacion de logica secuencial y paralela
    Co-authored-by: <WilmarSanchez>

--- src/ProyectoParaleloFinal/Program.cs
+++ src/ProyectoParaleloFinal/Program.cs
@@ -18,6 +18,33 @@ namespace ProyectoFinalModular
             Console.WriteLine($"Generando {CANTIDAD_PRODUCTOS:N0} registros...");
             var inventario = await Task.Run(() => InventarioDatos.Generar(CANTIDAD_PRODUCTOS));
 
+
+            // 2. SECUENCIAL
+            Console.WriteLine("\n-> Ejecutando Análisis SECUENCIAL...");
+            ResultadoPrediccion[] resSec = new ResultadoPrediccion[CANTIDAD_PRODUCTOS];
+            Stopwatch swSec = Stopwatch.StartNew();
+
+            for (int i = 0; i < inventario.Count; i++)
+            {               
+                resSec[i] = MotorEspeculativo.Procesar(inventario[i]);
+            }
+            swSec.Stop();
+            Console.WriteLine($"Tiempo: {swSec.ElapsedMilliseconds} ms");
+
+            // 3. PARALELO
+            Console.WriteLine("\n-> Ejecutando Análisis PARALELO...");
+            ResultadoPrediccion[] resPar = new ResultadoPrediccion[CANTIDAD_PRODUCTOS];
+            Stopwatch swPar = Stopwatch.StartNew();
+
+            Parallel.For(0, inventario.Count, i =>
+            {
+                resPar[i] = MotorEspeculativo.Procesar(inventario[i]);
+            });
+
+            swPar.Stop();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Tiempo: {swPar.ElapsedMilliseconds} ms");
+            Console.ResetColor();
         }
     }
 }
