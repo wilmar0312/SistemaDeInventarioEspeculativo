cb84097773daada9bc0ba7d85cefcbc0476f0fb3
wilmar sanchez <wilmarsanchez347@gmail.com>
2025-12-11 16:08:03 -0400

    feat: estructura inicial de program y generacion de datos
    Co-authored-by: <WilmarSanchez>

--- src/ProyectoParaleloFinal/Program.cs
+++ src/ProyectoParaleloFinal/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalModular
+{
+    class Program
+    {
+        const int CANTIDAD_PRODUCTOS = 2000000;
+
+        static async Task Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("=== SISTEMA INTEGRADO DE PREDICCIÓN PARALELA ===");
+            Console.ResetColor();
+
+            // 1. GENERACION
+            Console.WriteLine($"Generando {CANTIDAD_PRODUCTOS:N0} registros...");
+            var inventario = await Task.Run(() => InventarioDatos.Generar(CANTIDAD_PRODUCTOS));
+
+        }
+    }
+}
