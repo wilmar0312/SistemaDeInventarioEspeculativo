9366b460e5dc96098734631923f0986c0b8f904f
Carlos Reynoso <141469420+KizumoSuzuki@users.noreply.github.com>
2025-12-11 21:33:39 -0400

    Creación de lista de producto
    Co Authored by: Carlos Reynoso

---
+++ src/ProyectoParaleloFinal/InventarioDatos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProyectoFinalModular
+{
+    public class InventarioDatos
+    {
+        public static List<Producto> Generar(int cantidad)
+        {
+            List<Producto> productos = new List<Producto>();
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                productos.Add(new Producto
+                {
+                    Id = i,
+                    Categoria = "",
+                    StockActual = 0,
+                    VentasUltimosDias = Array.Empty<double>()
+                });
+            }
+
+            return productos;
+        }
+    }
+}
